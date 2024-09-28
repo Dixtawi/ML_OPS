15f6d3074cc1f48e4d0e260a8fa7444bd69a1599
Paul Giraud <paul.giraud@epita.fr>
2024-09-29 00:58:42 +0200

    feat:certificates and https works

--- frontend/app/__init__.py
+++ frontend/app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from .routes import routes
 import os
+import ssl
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.urandom(24)
     app.config.from_pyfile('../instance/config.py')
     app.config['SESSION_TYPE'] = 'filesystem'
+    
     app.register_blueprint(routes)
-    return app
+    
+    context = None
+    if not app.debug:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        context.load_cert_chain('certificates/cert.pem', 'certificates/key.pem')
+    
+    return app, context
--- frontend/instance/run.py
+++ frontend/instance/run.py
@@ -2,10 +2,11 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import create_app
+import ssl
 from config import API_PORT, API_HOST, DEBUG
 
 
-app = create_app()
+app, context = create_app()
 
 if __name__ == '__main__':
-    app.run(host=API_HOST, port=API_PORT, debug=DEBUG)
+    app.run(host=API_HOST, port=API_PORT, debug=DEBUG, ssl_context=context)
